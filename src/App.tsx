848cb2f0e7c35629f3b911a885af8caed1d24bd3
BrMD <dorigon.matheus8@gmail.com>
2023-09-22 16:33:54 -0300

    implementing sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,37 @@
-import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { db } from "../utils/db";
-import { onValue, ref } from "firebase/database";
+import { ref, get, child } from "firebase/database";
+
+import Home from "./pages/Home";
 
 import "./App.css";
 
 const queryClient = new QueryClient();
 
 function App() {
-  const { isLoading, error, data } = useQuery("repoData", () => {
-    fetch("https://api.github.com/repos/tannerlinsley/react-query").then(
-      (res) => res.json()
-    );
-  });
+  // const [teste, setTeste] = useState([Object]);
+  const dbRef = ref(db);
+  get(child(dbRef, "users"))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+      } else {
+        console.log("No data available");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 
+  // function writeUserData(userId: number, name: string) {
+  //   set(ref(db, "users/" + userId), {
+  //     username: name,
+  //   });
+  // }
+  // writeUserData(2, "teste33");
   return (
     <QueryClientProvider client={queryClient}>
-      <div>sla</div>
+      <Home />
     </QueryClientProvider>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import "../css/style.css";
+
+const Home = () => {
+  return (
+    <div className="containerHome">
+      <h1 className="headline headline--background-clip headline--linear-gradient">
+        Linear Gradient Text Stroke
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
